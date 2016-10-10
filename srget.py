39e367f9851db0ec28b961ae38f6bdc6dd8e622e
Pongwasin Wintakorn <pongwasin@hotmail.com>
2016-10-11 02:22:16 +0700

    Adding datasize getter and FileWrite function

--- srget.py
+++ srget.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from socket import socket,AF_INET,SOCK_STREAM
 
 class Downloader():
@@ -12,6 +13,7 @@ class Downloader():
         self.data = ""
         self.path =  "/Users/JarHan/Downloads"
         self.numConn = 5
+        self.contentLength = 0
     def connect(self):
         self.socket = socket(AF_INET,SOCK_STREAM)
         self.socket.connect((self.url,self.port))
@@ -34,7 +36,23 @@ class Downloader():
             if not buff:
                 break
             data += buff
-        self.data =data
+        self.data = data.split("\r\n\r\n")[-1]
         self.header = data.split("\r\n\r\n")[0]
+        self.socket.close()
+    def datasize (self):
+        header_list = self.header.split("\r\n")
+        for fields in header_list:
+            if "Content-Length" in fields:
+                cl = fields.split(":")[-1]
+                self.contentLength = int(cl)
+
     def WriteFile(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+        with open(os.path.join(path, self.FileName), 'wb') as f:
+                f.write(self.data)
+
+
+
 
