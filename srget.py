4d31600de88104eaf9183ac9db949b705de11a4b
Pongwasin Wintakorn <pongwasin@hotmail.com>
2016-10-11 01:28:19 +0700

    Adding input splitter function

--- srget.py
+++ srget.py
@@ -1,5 +1,4 @@
 import sys
-import urllib2
 from socket import socket,AF_INET,SOCK_STREAM
 
 class Downloader():
@@ -9,8 +8,23 @@ class Downloader():
         self.token = ""
         self.socket = socket(AF_INET,SOCK_STREAM)
         self.header = ""
+        self.FileName = ""
     def connect(self):
         self.socket = socket(AF_INET,SOCK_STREAM)
         self.socket.connect((self.url,self.port))
+    def GetHeader(self):
+        if "connect" == Argument:
+            self.header = "GET /auth HTTP/1.1\r\n"
+            self.header += "id: {}\r\n".format(self.id)
 
-    
+
+    def input_splitter(self,url):
+        self.FileName = url[2]
+        temp_url = url[3]
+        if ":" in temp_url:
+            self.DefaultPort = temp_url.split(":")[-1]
+        else:
+            self.url = temp_url
+
+    def
+
