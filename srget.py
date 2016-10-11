d470f9de4dff005529a40046febc08990f8ba9eb
Pongwasin Wintakorn <pongwasin@hotmail.com>
2016-10-11 18:40:54 +0700

    Increasing Speed

--- srget.py
+++ srget.py
@@ -40,7 +40,7 @@ class Downloader():
         data = ""
         header = ""
         while True:
-            buff = self.socket.recv(1024)
+            buff = self.socket.recv(8096)
             header += buff
 
             if "\r\n\r\n" in header:
@@ -54,16 +54,19 @@ class Downloader():
             if "Content-Length" in self.header:
                 self.datasize()
                 data_total = len(leftover)
+                # print self.content_length, data_total
+
                 while self.content_length > data_total:
-                    data_buff = self.socket.recv(1024)
+                    data_buff = self.socket.recv(8096)
                     f.write(data_buff)
 
                     data_total += len(data_buff)
+                    # print data_total
                 f.close()
             else:
                 total = 0
                 while True:
-                    data_buff = self.socket.recv(1024)
+                    data_buff = self.socket.recv(8096)
                     if not data_buff:
                         print "exiting loop"
                         break
@@ -72,7 +75,7 @@ class Downloader():
                 f.close()
                 print "Download Complete"
         self.socket.close()
-
+        sys.exit()
 
     def datasize (self):
         header_list = self.header.split("\r\n")
