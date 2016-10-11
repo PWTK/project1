04e017393dc0714db27cc3a223c77d8dac5bd679
Pongwasin Wintakorn <pongwasin@hotmail.com>
2016-10-11 13:50:45 +0700

    Combine file_write to recieve function

--- srget.py
+++ srget.py
@@ -1,58 +1,78 @@
 import sys
 import os
 from socket import socket,AF_INET,SOCK_STREAM
+from urlparse import urlparse
 
 class Downloader():
     def __init__(self):
-        self.url = ""
+        self.path = ""
+        self.host = ""
         self.socket = socket(AF_INET,SOCK_STREAM)
         self.header = ""
         self.FileName = ""
         self.data = ""
-        self.path =  "/Users/JarHan/Downloads"
+        self.pathdown =  "/Users/JarHan/Downloads"
         self.numConn = 5
-        self.contentLength = 0
+        self.content_length = 0
         self.port = 80
+        self.req_header = ""
+
     def connect(self):
         self.socket = socket(AF_INET,SOCK_STREAM)
-        self.socket.connect((self.url,self.port))
-    def send_request(self):
-        self.header = "GET /auth HTTP/1.1\r\n"
-        self.header += "{}:{}\r\n".format(self.url, self.port)
-    def input_splitter(self,url):
-        self.FileName = url[2]
+        self.socket.connect((self.host,self.port))
+        print "Connection Established"
 
-        temp_url = url[3]
+    def send_request(self):
+        self.req_header = "GET " + self.pathdown + " HTTP/1.1\r\n" + "Host: " + self.host + "\r\n\r\n"
+        self.socket.send(self.req_header)
+        print "Sending request header"
+
+    def input_splitter(self,argument):
+        self.FileName = argument[2]
+        print "Saving the file as: " + self.FileName
+        temp = argument[-1]
+        url = urlparse(temp)
+        self.host = url.hostname
+        self.path = url.path
         # if temp_url.split():
         #     self.DefaultPort = temp_url.split(":")[-1]
         # else:
-        self.url = temp_url
-
 
     def recv(self):
         data = ""
+        print "Start Receiving file"
         while True:
+            print "entering loop"
             buff = self.socket.recv(4068)
+            print buff
             if not buff:
+                print "exiting loop"
                 break
             data += buff
-        self.data = data.split("\r\n\r\n")[-1]
-        self.header = data.split("\r\n\r\n")[0]
+            split = data.split("\r\n\r\n")
+            leftover = split[-1]
+            data += leftover
+        header = split[0]
+        print "Data is" + data
+        self.header = header
+        # self.data = data.split("\r\n\r\n")[-1]
+        # self.header = data.split("\r\n\r\n")[0]
         self.socket.close()
-    def datasize (self):
-        header_list = self.header.split("\r\n")
-        for fields in header_list:
-            if "Content-Length" in fields:
-                cl = fields.split(":")[-1]
-                self.contentLength = int(cl)
 
-    def file_write(self):
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
+    # def datasize (self):
+    #     header_list = self.header.split("\r\n")
+    #     for fields in header_list:
+    #         if "Content-Length" in fields:
+    #             cl = fields.split(":")[-1]
+    #             self.content_length = int(cl)
 
-        with open(os.path.join(path, self.FileName), 'wb') as f:
-            f.write(self.data)
-            f.close()
+    # def file_write(self):
+    #     if not os.path.exists(self.path):
+    #         os.makedirs(self.path)
+    #     with open(os.path.join(self.path, self.FileName), 'wb') as f:
+    #         f.write(self.data)
+    #         f.close()
+
     def DownExec(self,argument):
         s = Downloader()
         s.input_splitter(argument)
